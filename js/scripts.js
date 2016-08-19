c08d0ddb3efa19b8310f5fddd306e82ef5294890
Blake Scala <blakescala@gmail.com>
2016-08-19 08:18:39 -0700

    print list of numbers up to input number in the output divider

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
-
-
+// Business logic
+var pingPong = (function(input){
+  var numbersArray = [];
+  for (var count = 1; count<=input; count++) {
+    numbersArray.push(count);
+  }
+  return numbersArray;
+});
 
 
 
 // User Interface
 $(document).ready(function() {
   $("form").submit(function(event) {
-    var inputNumber = $("#inputnumber").val();
-    console.log(inputNumber);
     event.preventDefault();
+    var inputNumber = parseInt($("#inputnumber").val());
+    var numbers = pingPong(inputNumber);
+    numbers.forEach(function(number) {
+      $("#output").append("<li>" + number + "</li>")
+    });
   });
 });
